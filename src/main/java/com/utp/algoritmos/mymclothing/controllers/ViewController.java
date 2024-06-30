d3568b4a97f6611187aabeca6881673acb9c1747
Al3x2004 <U22212317@utp.edu.pe>
2024-06-30 01:14:40 -0500

    Update ViewController.java

--- src/main/java/com/utp/algoritmos/mymclothing/controllers/ViewController.java
+++ src/main/java/com/utp/algoritmos/mymclothing/controllers/ViewController.java
@@ -1,1 +1,14 @@
+package com.utp.algoritmos.mymclothing.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Referencia a src/main/resources/templates/index.html
+    }
+}
 
